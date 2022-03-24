841a9ae56ac1c39904dc1b624361ef6ff88fee9b
shunwan <shun.wang@daimler.com>
2022-03-24 15:01:57 +0800

    ff4.0--ijk0.8.8--2022--0324--001

--- init-ios.sh
+++ init-ios.sh
@@ -19,7 +19,7 @@
 # IJK_FFMPEG_UPSTREAM=git://git.videolan.org/ffmpeg.git
 IJK_FFMPEG_UPSTREAM=https://github.com/Shunzi007/FFmpeg.git
 IJK_FFMPEG_FORK=https://github.com/Shunzi007/FFmpeg.git
-IJK_FFMPEG_COMMIT=ff4.0--ijk0.8.8--2022--0326--001
+IJK_FFMPEG_COMMIT=ff4.0--ijk0.8.8--2022--0324--001
 IJK_FFMPEG_LOCAL_REPO=extra/ffmpeg
 
 IJK_GASP_UPSTREAM=https://github.com/Bilibili/gas-preprocessor.git
